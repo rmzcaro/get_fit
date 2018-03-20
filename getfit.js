218d8ffef2a2d55b9e4747fafcfa7ae272f02db9
Carolina RC <caro.rmz315@gmail.com>
2018-03-20 18:41:45 -0500

    added second ajax call

--- getfit.js
+++ getfit.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 var apiKey = "c33c9593adf94c5e7fdc564c7cd0b1ff";
 var idAPI = "7cbf3fce";
 var queryURL = "https://trackapi.nutritionix.com/v2/search/instant?query=pizza";
+var queryURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
 // asynchronous HTTP request
 
 
@@ -21,8 +22,20 @@ $.ajax({
     "x-remote-user-id": 0
     }
 }).then(function(response) {
-    console.log(queryURL);
-    console.log(response);
+$.ajax({
+    url: queryURL1, 
+    method: "POST",
+    dataType: "json",
+    headers: {
+    "x-app-id": idAPI,
+    "x-app-key": apiKey,
+    "x-remote-user-id": 0
+
+    // console.log(queryURL1);
+    // console.log(response);
+
+   
+    }
 
     // Add here AJAX call to get the exercise needed to burn 
     // console(response.RunTime);
