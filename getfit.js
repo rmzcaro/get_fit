54ee04afc23f11f5b90d487e1ae2335620ef240f
Carolina RC <caro.rmz315@gmail.com>
2018-03-19 22:34:40 -0500

    ajax request and response

--- getfit.js
+++ getfit.js
@@ -3,18 +3,29 @@
 
 //application id: 7cbf3fce
 //application key: c33c9593adf94c5e7fdc564c7cd0b1ff
+$(document).ready(function(){
 
-$(document).ready(function() {
+var apiKey = "c33c9593adf94c5e7fdc564c7cd0b1ff";
+var idAPI = "7cbf3fce";
+var queryURL = "https://trackapi.nutritionix.com/v2/search/instant?query=pizza";
+// asynchronous HTTP request
 
-var title = "nutritionix-test";
-var queryURL = "https://nutritionix.com/api/index.php?method=request.get&accesskey=c33c9593adf94c5e7fdc564c7cd0b1ff";
 
 $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
+    dataType: "json",
+    headers: {
+    "x-app-id": idAPI,
+    "x-app-key": apiKey,
+    "x-remote-user-id": 0
+    }
 }).then(function(response) {
+    console.log(queryURL);
     console.log(response);
-    console(response.RunTime);
-}); 
+
+    // Add here AJAX call to get the exercise needed to burn 
+    // console(response.RunTime);
+});
 
 });
