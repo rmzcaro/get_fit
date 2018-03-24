fa72c8a8eb8f81b348c6fbcf76910053476a925b
Carolina RC <caro.rmz315@gmail.com>
2018-03-23 22:52:26 -0500

    three ajax calls, console log nutrition to browser

--- getfit.js
+++ getfit.js
@@ -1,26 +1,28 @@
 // jQuery var we will use insted of the literal string inside 
 // the ajax call 
 
-//application id: 7cbf3fce
-//application key: c33c9593adf94c5e7fdc564c7cd0b1ff
+// application id: 7cbf3fce
+// application key: c33c9593adf94c5e7fdc564c7cd0b1ff
+
 $(document).ready(function () {
 
             var apiKey = "c33c9593adf94c5e7fdc564c7cd0b1ff";
             var idAPI = "7cbf3fce";
 
-            //EVENT LISTENER
+            var userInput;
+            // EVENT LISTENER
 
             // The on click event that gets the users input 
             $(".image").on("click", function () {
                 event.preventDefault();
                 // Get the value associated with the button the user picked
                 // REVIEW THIS 
-                var userInput = $(this).val();
-            });
+                userInput = $(this).attr("name");
+                console.log(userInput);
 
             // queryURL must be able to accept different entries. 
             // CURRENTLY IT ONLY ACCEPTS 1 QUERY 
-            var queryURL = "https://trackapi.nutritionix.com/v2/search/instant?query=hotdog";
+            var queryURL = "https://trackapi.nutritionix.com/v2/search/instant?query=" + userInput;
             var queryURL1 = "https://trackapi.nutritionix.com/v2/natural/nutrients";
             // asynchronous HTTP request
 
@@ -37,49 +39,47 @@ $(document).ready(function () {
                 //After the data comes back from the API
             }).then(function (response) {
                 console.log(response);
-                console.log(response.branded[0].food_name);
-                console.log(response.branded[0].nf_calories);
+                // console.log(response.branded[0].food_name);
+                // console.log(response.branded[0].nf_calories);
 
                 // creates var for storing response 
                 var food_name = response.common[0].food_name;
                 var img = response.common[0].photo.thumb;
                 var servingQty = response.common[0].serving_qty;
                 var servingUnit = response.common[0].serving_unit;
+                console.log("food ="+ food_name,"img ="+img, "servingQty ="+servingQty, "servingUnit ="+ servingUnit);
 
                 $.ajax({
                     url: queryURL1,
                     data: {
-                        "query": "apple",
+                        "query": food_name,
                         "num_servings": 1,
-                        "aggregate": "apples",
+                        "aggregate": food_name,
                         "line_delimited": false,
                         "use_raw_foods": true,
                     },
                     method: "POST",
-                    dataType: "application/json",
+                    ContentType: "application/json",
                     headers: {
                         "x-app-id": idAPI,
                         "x-app-key": apiKey,
                         "x-remote-user-id": 0
                     }
 
+                }).then(function (nutrition) {
+                    console.log(nutrition);
+
+                    //WE NEED TO CONSOLE LOG CALORIES HERE 
+                    //save variable for calories
+                    var calories = nutrition.foods[0].metadata.nf_calories;
+                    console.log(calories);
+
                 });
+        });
 
-                // push info into firebase 
-                database.ref().push(food_name);
-                database.ref().push(img);
-                database.ref().push(servingQty);            
-                database.ref().push(servingUnit);
-
-                alert("Food added!");
-                // name: name, 
-
-                // });
-
-                // return the calories 
-
-                // show on browser
 
             });
 
-        });
+            //DO WE NEED TO ADD ERROR HANDLER 
+
+    });
---
+++ savethis.js
@@ -0,0 +1,21 @@
+
+         
+
+                // thisi is not the code you want to use, this is just a placeholder 
+
+                // push info into firebase 
+                database.ref().push(food_name);
+                // database.ref().push(img);
+                // database.ref().push(servingQty);            
+                // database.ref().push(servingUnit);
+
+                alert("Food added!");
+                // name: name, 
+
+                // });
+
+                // return the calories 
+
+                // show on browser
+
+    
